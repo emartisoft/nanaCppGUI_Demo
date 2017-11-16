41f0417f61ebe567f119286d366afc6f9902c023
emarti <dtemarti@gmail.com>
2017-11-16 18:04:06 +0300

    Update NanaProjectApp.cpp
    Added setting icon to form (alternative solution)

--- NanaProject/NanaProjectApp.cpp
+++ NanaProject/NanaProjectApp.cpp
@@ -1,9 +1,20 @@
 #include "NanaProjectApp.h"
 
+using namespace std;
+
 int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
 {
 	MainForm mainForm;
-	mainForm.icon(paint::image("app.ico"));
+	
+	// If you wanna use "app.ico" named icon file in the same folder
+	//mainForm.icon(paint::image("app.ico")); 
+	
+	// Thanks to Error Flynn from Nana C++ Library Forum
+	wstring app_path(4096, '\0');
+	app_path.resize(GetModuleFileNameW(0, &app_path.front(), app_path.size()));
+	mainForm.icon(paint::image(app_path));
+	// End of thanks :)
+	
 	mainForm.show();
 	exec();
 }
