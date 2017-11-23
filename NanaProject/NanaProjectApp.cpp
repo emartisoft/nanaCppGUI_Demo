7d0e3f3d0a8ba694074ba9b03dbb6a458f4e71b8
emarti <dtemarti@gmail.com>
2017-11-24 00:30:48 +0300

    Update NanaProjectApp.cpp

--- NanaProject/NanaProjectApp.cpp
+++ NanaProject/NanaProjectApp.cpp
@@ -7,7 +7,9 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int n
 	MainForm mainForm;
 	
 	// If you wanna use "app.ico" named icon file in the same folder
-	//mainForm.icon(paint::image("app.ico")); 
+	//mainForm.icon(paint::image("app.ico"));
+	// or your project exe name (this project's exe name is 'NanaProject.exe')
+	//mainForm.icon(paint::image("NanaProject.exe"));
 	
 	// Thanks to Error Flynn from Nana C++ Library Forum
 	wstring app_path(4096, '\0');
